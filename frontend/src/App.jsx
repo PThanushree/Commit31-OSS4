a987d95b60ce02350ad2910bfa8f3b5bada9791a
M GANAADITHYA <ganaadithya.m@gmail.com>
2026-03-03 12:49:06 +0530

    feat: registeration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
+import Register from "./pages/register";
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-600">
         Student Developer Platform 🚀
       </h1>
+      <div className="w-full max-w-md">
+        <Register/>
+      </div>
+    
+          
     </div>
   );
 }
